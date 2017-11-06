2217f22431ba305438baad29cbf0d699df35f21c
Aziz <aalzamel@gmail.com>
2017-11-06 21:00:01 +0300

    changed some styles, and the URL links methods

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -9,3 +10,7 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+	def get_absolute_url(self):
+		return reverse("detail", kwargs={"post_id":self.id})
