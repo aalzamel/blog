d3e7708d293c2df09c8a6c38c03fad0bdbd28cc1
Aziz <aalzamel@gmail.com>
2017-11-09 17:25:43 +0300

    boot strap and base.html are now in use

--- posts/views.py
+++ posts/views.py
@@ -1,33 +1,105 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
-# Create your views here.
-
+# Test post model
 
 def post(request):
 	context = {
-	"firstname": "Aziz",
-	"lastname": "Alzamel",
-	"auser": request.user.username,
+		"firstname": "Aziz",
+		"lastname": "Alzamel",
+		"auser": request.user.username,
 	}
 
 	return render(request, "home.html", context)
 
 
+
+# List view of the blog posts
+
 def post_list(request):
 	thelist  = Post.objects.all()
 	context = {
-	"post_items": thelist,
+		"post_items": thelist,
 	}
 
 	return render(request, "post_list.html", context)
 
 
+
+# Detailed page of the post
+
 def post_detail(request, post_id):
 	#item = Post.objects.get(id=1000)
 	item = get_object_or_404(Post, id=post_id)
 	context = {
-	"items": item,
+		"items": item,
 	}
 
 	return render(request, "detail.html", context)
+
+
+
+#def post_create(request):
+#	form = PostForm(request.POST or None)
+#	if form.is_valid():
+#		form.save()
+#		return redirect("posts:post_list")
+#	context = {
+#		"form": form
+#	}
+#
+#	return render(request, "post_create.html", context)
+
+
+
+# Creating a post
+
+def post_create(request):
+	form = PostForm()
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			form.save()
+			messages.success(request, " Awesome, a blog post has been added")
+			return redirect("posts:post_list")
+	context = {
+		"form": form
+	}
+
+	return render(request, "post_create.html", context)
+
+
+
+# Updating a post
+
+def post_update(request,post_id):
+	item = Post.objects.get(id=post_id)
+
+	form = PostForm(request.POST or None, instance=item)
+
+	if form.is_valid():
+		form.save()
+		messages.info(request, "Blog post updated")
+		return redirect("posts:post_list")
+	
+	context = {
+		"form": form,
+		"item": item,
+	}
+
+	return render(request, "post_update.html", context)
+
+
+# Deleting a post
+
+def post_delete(request,post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "Post have been deleted")
+	return redirect("posts:post_list")
+
+
+
+
+
